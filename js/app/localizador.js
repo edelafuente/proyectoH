19dc5f8c89fd11737b22341ecc001cdce23acff5
alexbr86 <abalseiro001@gmail.com>
2014-03-25 18:06:34 +0100

    prueba alex

---
+++ js/app/localizador.js
@@ -0,0 +1,25 @@
+﻿var App = App || {};
+App.localizador = (function (){
+    'use strict';
+
+    var localizate = function(e, Radio, GPS, success){
+        $.ajax({
+            url : '../../data/datos.json',
+            type : 'POST',
+            data: {radio : Radio},
+            dataType : 'json',
+            cache : false,  
+            success : function(data){
+                success(data);
+            },
+            error : function(jqXHR, textStatus, errorThrown){
+                console.log(errorThrown);
+            }
+        });
+
+    };
+
+    return {
+        localizate : localizate
+    };
+})();
