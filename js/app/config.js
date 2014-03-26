0ce04ca30e43a171ddb9016f5c923899d7fceb49
IMadina <inigomad@hotmail.com>
2014-03-26 15:37:27 +0100

    Config

--- js/app/config.js
+++ js/app/config.js
@@ -2,5 +2,32 @@ var App = App||{};
 App.Config = (function() {
     "use strict";
 
+    var radio = function(){
+
+        $.ajax({
+            url : '../../data/servidor/maquinas.json',
+            type : 'POST',
+            data : {},
+            dataType : 'json',
+            cache : false,  
+            success : function(data){
+                console.log(data);
+                return(data);
+            },
+            error : function(jqXHR, textStatus, errorThrown){
+                console.log(errorThrown);
+            }
+        });
+
+    };
+
+    radio();
+
+    return {
+        radio : radio
+        //filtros : filtros
+        
+    };
+
 
 })();
