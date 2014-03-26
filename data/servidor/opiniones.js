afe4fbab7d1a15a15f617e7cdddfd33f390d16be
0sv <arenedo@gmail.com>
2014-03-26 16:58:09 +0100

    rellenar los comentarios

--- data/servidor/opiniones.js
+++ data/servidor/opiniones.js
@@ -1,9 +1,45 @@
 [{
-    "id": 1,
-    "texto": "Esta maquina mola.",
-    "idmaquina": "1",
-    "idusuario": "1",
+        "id": 1,
+        "texto": "Esta maquina mola.",
+        "idmaquina": "1",
+        "idusuario": "1",
+        "like": 1
+    }, {
+
+        "id": 2,
+        "texto": "Esta maquina no mola.",
+        "idmaquina": "6",
+        "idusuario": "1",
+        "like": 7
 
 
+    }, {
+
+        "id": 3,
+        "texto": "Esta maquina no mola.",
+        "idmaquina": "1",
+        "idusuario": "3",
+        "like": -5
+
+
+    }, {
+
+        "id": 4,
+        "texto": "Esta maquina no mola.",
+        "idmaquina": "6",
+        "idusuario": "2",
+        "like": 0
+
+
+    }, {
+
+        "id": 5,
+        "texto": "Esta maquina no mola.",
+        "idmaquina": "3",
+        "idusuario": "2",
+        "like": -1
+
+
+    }
 
 }]
