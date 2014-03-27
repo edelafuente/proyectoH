4635906d2287a7873f01529c70d19a3673c2ac66
IMadina <inigomad@hotmail.com>
2014-03-27 18:15:23 +0100

    t

--- js/app/ui.js
+++ js/app/ui.js
@@ -4,56 +4,6 @@ App.Ui = (function() {
 
     var mostrarMaquinas = function(maquinas){
 
-        maquinas = [{
-        "id": "1",
-        "ubicacion": {
-            "longitud": 43.27633,
-            "latitud": -2.024574
-        },
-        "contenido": ["comidacaliente", "bebidacaliente", "comidafria", "bebidafria", "otros"],
-        "urlimagen": "maq3.jpg"
-    }, {
-        "id": "2",
-        "ubicacion": {
-            "longitud": 43.309628,
-            "latitud": -1.983204
-        },
-        "contenido": ["comidafria", "bebidafria"],
-        "urlimagen": "maq4.jpg"
-    }, {
-        "id": "3",
-        "ubicacion": {
-            "longitud": 43.321119,
-            "latitud": -1.982775
-        },
-        "contenido": ["comidacaliente", "bebidafria"],
-        "urlimagen": "maq1.jpg"
-    },{
-        "id": "4",
-        "ubicacion": {
-            "longitud": 43.27633,
-            "latitud": -2.024574
-        },
-        "contenido": ["comidacaliente", "bebidacaliente", "comidafria", "bebidafria", "otros"],
-        "urlimagen": "maq3.jpg"
-    }, {
-        "id": "5",
-        "ubicacion": {
-            "longitud": 43.309628,
-            "latitud": -1.983204
-        },
-        "contenido": ["comidafria", "bebidafria"],
-        "urlimagen": "maq4.jpg"
-    }, {
-        "id": "6",
-        "ubicacion": {
-            "longitud": 43.321119,
-            "latitud": -1.982775
-        },
-        "contenido": ["comidacaliente", "bebidafria"],
-        "urlimagen": "maq1.jpg"
-    }];
-
         var lista = $('<ul/>', {class : 'list-group'});
 
         for (var i = maquinas.length - 1; i >= 0; i--) {
@@ -69,8 +19,6 @@ App.Ui = (function() {
 
         }
 
-        $(lista).appendTo('#inicio');
-
         return lista;
 
     };
@@ -82,8 +30,6 @@ App.Ui = (function() {
         return detalle;
 
     };
-
-    mostrarMaquinas();
 
     return {
         mostrarMaquinas : mostrarMaquinas,
