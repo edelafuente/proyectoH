ae49c7d3577e9bd5055fa2ca1a306292c248c156
edelafuente <storm.dlf@gmail.com>
2014-03-27 16:41:34 +0100

    visual

--- js/app/ui.js
+++ js/app/ui.js
@@ -4,10 +4,10 @@ App.Ui = (function() {
 
     var mostrarMaquinas = function(maquinas){
 
-        var lista = $('<ul/>', {class : 'lista'});
+        var lista = $('<ul/>', {class : 'list-group'});
 
         for (var i = maquinas.length - 1; i >= 0; i--) {
-            lista.append('<li data-id="' + maquinas[i].id + '"><img src="img/'+ maquinas[i].urlimagen.replace(".jpg","_min.jpg") +'" style="width:10%"><span>Contenido:' + maquinas[i].contenido + '</li>');
+            lista.append('<li class="list-group-item" data-id="' + maquinas[i].id + '"><img src="img/'+ maquinas[i].urlimagen.replace(".jpg","_min.jpg") +'" style="width:10%"><span>Contenido:' + maquinas[i].contenido + '</li>');
         }
 
     };
